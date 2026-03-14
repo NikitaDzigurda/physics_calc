3aba15a4308d1baa86caef7ff633c29ce8d46c53
Nikita_Dzigurda <ghataju@yandex.ru>
2026-03-14 23:35:58 +0300

    replace format of output data (first steps for UI)

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <string>
+
 
 
 struct SizeException{
@@ -72,9 +74,33 @@ class Dimensions{
         }
 };
 
+// std::ostream& operator<<(std::ostream& os, const Dimensions& d) {
+//     const auto v = d.GetDegree();
+//     os << '[' << v[0] << ',' << v[1] << ',' << v[2] << ']';
+//     return os;
+// }
+
 std::ostream& operator<<(std::ostream& os, const Dimensions& d) {
+    std::string result;
+    std::vector<std::string> si {"kg", "s", "m"};
+
     const auto v = d.GetDegree();
-    os << '[' << v[0] << ',' << v[1] << ',' << v[2] << ']';
+    for (size_t i=0; i < v.size(); ++i){
+        if (v[i] != 0){
+            if (v[i] == 1){
+                result += (si[i] + '*');
+            }
+            else{
+                result += (si[i] + '^' + std::to_string(v[i]) + '*');
+            }
+        }
+    }
+    if (result.empty()) {
+        os << "1";
+    } else {
+        result.pop_back();
+        os << result;
+    }
     return os;
 }
 
@@ -129,7 +155,7 @@ class Quantity{
             return *this;
         }
 
-        Quantity operator+(const Quantity& other){
+        Quantity operator+(const Quantity& other) const{
             if (other.dims != dims){
                 throw DimensionsException{};
             }
@@ -139,7 +165,7 @@ class Quantity{
             return result;
         }
 
-        Quantity operator-(const Quantity& other){
+        Quantity operator-(const Quantity& other) const{
             if (other.dims != dims){
                 throw DimensionsException{};
             }
@@ -159,11 +185,6 @@ Quantity<T> operator*(const T& scalar, const Quantity<T>& q) {
     return q * scalar;
 }
 
-template <typename T>
-Quantity<T> operator/(const T& scalar, const Quantity<T>& q) {
-    return q / scalar;
-}
-
 template<typename T>
 std::ostream& operator<<(std::ostream& os, const Quantity<T>& n){
     const auto num = n.GetValue();
@@ -177,6 +198,10 @@ struct Base_dimens{
     Dimensions time_base {{0, 1, 0}};
     Dimensions length_base {{0, 0, 1}};
 };
+
+const Quantity<double> KG(1.0, Dimensions({1, 0, 0}));
+const Quantity<double> SECOND(1.0, Dimensions({0, 1, 0}));
+const Quantity<double> METER(1.0, Dimensions({0, 0, 1}));
 
 
 int main() {
@@ -201,20 +226,30 @@ int main() {
 
     // Проверка операций сложения-вычетания величин одинаковой и разной размерности (также с константой)
 
-    Base_dimens base_dimens;
-    Quantity res1(10, base_dimens.length_base);
-    Quantity res2(20, base_dimens.length_base);
+    // Base_dimens base_dimens;
+    // Quantity res1(10, base_dimens.length_base);
+    // Quantity res2(20, base_dimens.length_base);
 
-    Quantity res3(30, base_dimens.time_base);
-    Quantity res4(40, base_dimens.time_base);
+    // Quantity res3(30, base_dimens.time_base);
+    // Quantity res4(40, base_dimens.time_base);
 
-    auto totaldist1 = res1 + res2;
-    // auto totaldist2 = res1 + res3; // - поймаем ошибку поскольку разные размерности
-    auto totaldist3 = res4 - res3;
-    auto halfdist = totaldist3 / 2.0;
+    // auto totaldist1 = res1 + res2;
+    // // auto totaldist2 = res1 + res3; // - поймаем ошибку поскольку разные размерности
+    // auto totaldist3 = res4 - res3;
+    // auto halfdist = totaldist3 / 2.0;
 
-    std:: cout << totaldist1 << "\n";
-    // std:: cout << totaldist2 << "\n";
-    std:: cout << totaldist3 << "\n";
-    std:: cout << halfdist << "\n";
+    // std:: cout << totaldist1 << "\n";
+    // // std:: cout << totaldist2 << "\n";
+    // std:: cout << totaldist3 << "\n";
+    // std:: cout << halfdist << "\n";
+
+    // Проверка вывода результата с помощью размерности из величин системы СИ 
+
+    std:: cout << KG * METER / SECOND / SECOND << "\n"; // подсчет силы с помощью константных данных
+
+    auto mass = 70.0 * KG;
+    auto acceleration = 9.8 * METER / (SECOND * SECOND);
+    auto force = mass * acceleration;
+
+    std::cout << "Force: " << force << std::endl;
 }
